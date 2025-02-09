3a9d07c8b4bdbd0e70c84c8d986c7d5341c60435
kik8727 <kik87827@gmail.com>
2025-02-09 18:02:43 +0900

    스터디중

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,23 +1,43 @@
-'use client';
+"use client";
 
-import { age, name } from './data';
+import { age, name } from "./data";
 
 export default function Cart() {
+  let cartData = ["Tomatoes", "Pasta"];
   return (
     <div>
-      <h4 className="title">
-        Cart {age} {name}
-      </h4>
-      <CartItem />
-      <CartItem />
+      <h4 className="title">Cart</h4>
+      {cartData.map((data, index) => (
+        <CartItem itemData={data} key={data} />
+      ))}
+      <Banner title="롯데카드" />
+      <Banner title="삼성카드" />
+      <div style={{ textAlign: "center" }}>
+        <Button />
+        <Button theme="red" />
+        <Button theme="blue" />
+      </div>
     </div>
   );
 }
 
-function CartItem() {
+function Banner({ title }) {
+  return <h5>{title} 결제 행사중</h5>;
+}
+
+function Button({ theme }) {
+  theme = !!theme ? theme : "grey";
+  return (
+    <button style={{ background: theme, padding: 10, color: "#fff" }}>
+      버튼
+    </button>
+  );
+}
+
+function CartItem({ itemData }) {
   return (
     <div className="cart-item">
-      <p>상품명</p>
+      <p>{itemData}</p>
       <p>$40</p>
       <p>1개</p>
     </div>
--- app/list/page.js
+++ app/list/page.js
@@ -1,19 +1,27 @@
-import Image from 'next/image';
-import food0 from '/public/food0.png';
+"use client";
 
+import Image from "next/image";
+import { useEffect, useState } from "react";
 export default function List() {
-  let product = ['Tomatoes', 'Pasta', 'Coconut'];
+  let [productData] = useState(["Tomatoes", "Pasta", "Coconut"]);
+  let [count, setCount] = useState(() => productData.map(() => 0));
 
-  console.log(product);
+  const updateCount = (index, value) => {
+    setCount((prev) =>
+      prev.map((c, i) => (i === index ? Math.max(0, c + value) : c))
+    );
+  };
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      {product.map((item, index) => {
+      {productData.map((item, index) => {
         return (
           <div className="food" key={item}>
-            <img src={`/food${index}.png`} className="food-img" alt={item} />
-            {/* <Image src={food0} className="food-img" alt={item} /> */}
+            <img src={`/food${index}.png`} className="food-img" alt="" />
             <h4>{item} $40</h4>
+            <span> {count[index]} </span>
+            <button onClick={() => updateCount(index, 1)}>+</button>
+            <button onClick={() => updateCount(index, -1)}>-</button>
           </div>
         );
       })}
